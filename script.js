e53e66a7861deb1c54f4186bd05be8f884e393fe
Marcelo <marcelo100296@gmail.com>
2026-04-21 23:45:51 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,73 @@
+const menu = document.querySelector("#menu")
+const form = menu.querySelector("form")
+const botao = menu.querySelector("button")
+const inputMembros = document.querySelector("#membros")
+const inputRefeicao = document.querySelector("#refeicao")
+let valores = []
+const tabela = document.querySelector("#escadinha")
+const tbody = tabela.querySelector("tbody")
+const total = tabela.querySelector(".total")
+
+botao.addEventListener("click", function (evento) {
+  if (!form.checkValidity()) return
+
+  evento.preventDefault()
+  valores = escadinha(inputMembros, inputRefeicao)
+  deletarTabela()
+  criarTabela(inputMembros)
+
+  tabela.classList.remove("esconder")
+  alert("Escadinha da Brena calculada!")
+})
+
+function deletarTabela() {
+  const trs = tbody.querySelectorAll("tr")
+
+  if (trs) trs.forEach(tr => tr.remove())
+}
+
+function criarTabela(inputMembros) {
+  const membros = inputMembros.value
+
+  for (let i = 0; i < valores.length; i++) {
+    const tr = document.createElement("tr")
+    tbody.append(tr)
+
+    const tdMembro = document.createElement("td")
+    tdMembro.textContent = i + 2 + "º"
+    tr.append(tdMembro)
+
+    const tdValor = document.createElement("td")
+    tdValor.textContent = valores[i]
+    tr.append(tdValor)
+  }
+
+  total.textContent = inputRefeicao.value
+}
+
+function escadinha(inputMembros, inputRefeicao) {
+  const membros = Number(inputMembros.value)
+  const refeicao = Number(inputRefeicao.value)
+  const valores = []
+
+  let porcentagem = 1
+  let divisor = 0
+
+  for (let i = 0; i < membros; i++) {
+    divisor += porcentagem
+    porcentagem *= 1.1
+  }
+
+  let preco = refeicao / divisor
+  let total = preco
+
+  for (let i = 1; i < membros; i++) {
+    const valor = Number(preco.toFixed(2))
+    total += preco *= 1.1
+    valores.push(valor)
+  }
+  console.log(valores)
+  console.log(total)
+
+  return valores
+}
